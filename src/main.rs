e8a7e1c303a663fef3cc9b5deb081139cff18d87
Myk Melez <myk@mykzilla.org>
2018-02-06 15:31:49 -0800

    initial fern program

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+struct Fern {
+    size: f64,
+    growth_rate: f64,
+}
+
+impl Fern {
+    /// Simulate a fern growing for one day.
+    fn grow(&mut self) {
+        self.size *= 1.0 + self.growth_rate;
+    }
+}
+
+/// Run a fern simulation for some number of days.
+fn run_simulation(fern: &mut Fern, days: usize) {
+    for _ in 0 .. days {
+        fern.grow();
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut fern = Fern {
+        size: 1.0,
+        growth_rate: 0.001,
+    };
+    run_simulation(&mut fern, 1000);
+    println!("final fern size: {}", fern.size);
 }
